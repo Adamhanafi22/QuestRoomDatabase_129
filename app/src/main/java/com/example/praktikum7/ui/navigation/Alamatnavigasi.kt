0040f04954c3c693cc08395aca0511bd06c79173
Adamhanafi22 <adamhanafi1922@gmail.com>
2024-12-16 14:16:42 +0700

    untuk menandai view

--- app/src/main/java/com/example/praktikum7/ui/navigation/Alamatnavigasi.kt
+++ app/src/main/java/com/example/praktikum7/ui/navigation/Alamatnavigasi.kt
@@ -3,3 +3,18 @@ package com.example.praktikum7.ui.navigation
 interface Alamatnavigasi {
     val route: String
 }
+ // untuk menandai view
+object DestinasiHome : Alamatnavigasi {
+    override val route = "home"
+}
+object DestinasiDetail : Alamatnavigasi {
+    override val route = "Detail"
+    const val NIM = "nim"
+    val routeWithArg = "$route/{$NIM}"
+}
+object DestinasiUpdate : Alamatnavigasi{
+    override val route = "update"
+    const val NIM = "nim"
+    val routeWithArg = "nim"
+
+}
