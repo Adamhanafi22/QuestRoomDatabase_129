29c2b3cc446b1890cccb806fafc19d4f5c0d1640
Adamhanafi22 <adamhanafi1922@gmail.com>
2024-12-16 13:50:20 +0700

    Membuat Fungsi data base

--- app/src/main/java/com/example/praktikum7/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/praktikum7/repository/RepositoryMhs.kt
@@ -1,8 +1,20 @@
 package com.example.praktikum7.repository
 
 import com.example.praktikum7.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
 
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    fun getAllMhs() : Flow<List<Mahasiswa>> // Memanggil fungsi getAllMahasiswa dari MahasiswaDao
+
+    fun getMhs(nim: String): Flow<Mahasiswa> // Untuk Mengammbil data dari MahasiswaDao berdasarkan Nim
+
+    suspend fun deletedMhs (mahasiswa: Mahasiswa) // untuk menghapus data dari MahasiswaDao
+
+    suspend fun updateMhs(mahasiswa: Mahasiswa) //untuk update data dari MahasiswaDao
+
+
+
 }
