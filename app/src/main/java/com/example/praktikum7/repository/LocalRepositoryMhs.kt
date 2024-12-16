1a5db9cad0a9a733f15e87c954c9b1494b148f18
Adamhanafi22 <adamhanafi1922@gmail.com>
2024-12-16 14:06:28 +0700

    menghubungkan MahasiswaDao dengan operasi yang didefinisikan dalam RepositoryMhs

--- app/src/main/java/com/example/praktikum7/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/praktikum7/repository/LocalRepositoryMhs.kt
@@ -2,6 +2,7 @@ package com.example.praktikum7.repository
 
 import com.example.praktikum7.data.dao.MahasiswaDao
 import com.example.praktikum7.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs(
     private val mahasiswaDao: MahasiswaDao
@@ -9,5 +10,21 @@ class LocalRepositoryMhs(
 
     override suspend fun insertMhs(mahasiswa: Mahasiswa){
         mahasiswaDao.insertMahasiswa(mahasiswa)
+    }
+    // getAllMhs
+    override fun getAllMhs (): Flow<List<Mahasiswa>> {
+        return  mahasiswaDao.getAllMahasiswa()
+    }
+    // getMhs
+    override fun getMhs(nim: String): Flow<Mahasiswa> {
+        return mahasiswaDao.getMahasiswa(nim)
+    }
+    // deletedMhs
+    override suspend fun deletedMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.deletedMAhasiswa(mahasiswa)
+    }
+    // updateMhs
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.updateMahasiswa(mahasiswa)
     }
 }
