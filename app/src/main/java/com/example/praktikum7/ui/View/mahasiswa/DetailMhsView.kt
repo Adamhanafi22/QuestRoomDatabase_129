d44e5bc88ed925d4cbfac9e84353d37ae35ee206
Adamhanafi22 <adamhanafi1922@gmail.com>
2024-12-16 19:14:06 +0700

    DetailMhsView(DeleteConfirmationDialog)

--- app/src/main/java/com/example/praktikum7/ui/View/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/praktikum7/ui/View/mahasiswa/DetailMhsView.kt
@@ -1,2 +1,28 @@
 package com.example.praktikum7.ui.View.mahasiswa
 
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit, onDeleteCacel: () -> Unit, modifier: Modifier =
+        Modifier
+) {
+    AlertDialog(onDismissRequest = { /* Do Nothing */},
+        title = { Text("Delete Data") },
+        text = { Text("Apakah anda yakin ingin menghapus data?") },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCacel) {
+                Text(text = "Cancel")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Yes")
+            }
+        })
+}
