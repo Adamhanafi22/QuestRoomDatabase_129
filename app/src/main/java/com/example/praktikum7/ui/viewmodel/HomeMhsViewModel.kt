29bd766d908e28159cf255051acd7a74cbb5648d
Adamhanafi22 <adamhanafi1922@gmail.com>
2024-12-16 14:30:22 +0700

    Membuat class HomeMhsviewModel

--- app/src/main/java/com/example/praktikum7/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/praktikum7/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,6 +1,57 @@
 package com.example.praktikum7.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.praktikum7.data.entity.Mahasiswa
+import com.example.praktikum7.repository.LocalRepositoryMhs
+import com.example.praktikum7.repository.RepositoryMhs
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class HomeMhsViewModel(
+    private val repositoryMhs: RepositoryMhs
+
+) : ViewModel() {
+
+    val homeUiState: StateFlow<HomeUiState> = repositoryMhs.getAllMhs()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listMhs = it.toList(),
+                isLoading = false,
+            )
+
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit (
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope ,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState (
+                isLoading = true,
+            )
+        )
+
+
+}
 
 data class HomeUiState (
     val listMhs: List<Mahasiswa> = listOf(),
@@ -8,4 +59,5 @@ data class HomeUiState (
     val isError: Boolean = false,
     val errorMessage: String = ""
 )
+
 
